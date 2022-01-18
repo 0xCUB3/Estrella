692a9bdb48a4e5c1350d79ba179d1bac0109b670
0xCUBE <94565160+0xCUB3@users.noreply.github.com>
2022-01-18 15:32:27 -0500

    Add join and leave messages

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ from discord.ext import commands
 # Import Bot Token
 from vars import *
 
-client = commands.Bot(command_prefix=prefix)
+intents = discord.Intents.default()
+intents.members = True
+
+client = commands.Bot(command_prefix=prefix, intents=intents)
 
 @client.event
 async def on_ready():
@@ -16,5 +19,15 @@ async def on_ready():
 @client.command()
 async def hello(ctx):
     await ctx.send("Hello!")
+
+@client.event
+async def on_member_join(member):
+    channel = client.get_channel(welcome_channel_id)
+    await channel.send("Hello!")
+
+@client.event
+async def on_member_remove(member):
+    channel = client.get_channel(goodbye_channel_id)
+    await channel.send("Goodbye!")
 
 client.run(token)
--- vars.py
+++ vars.py
@@ -1,3 +1,5 @@
 # Variables Used by the Bot
 prefix = '$'
 token = 'OTMyNzAzNzA0NDU5NjYxMzMy.YeW2Aw.PCYy840HyqRbLCOWjY-nynfOlck'
+welcome_channel_id = 932713007761489963
+goodbye_channel_id = 932713007761489963
