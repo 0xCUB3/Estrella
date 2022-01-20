503712896ec6e01e11b214d525e6510975fa94f3
0xCUBE <94565160+0xCUB3@users.noreply.github.com>
2022-01-20 12:41:10 -0500

    Add VC join/leave

--- main.py
+++ main.py
@@ -30,4 +30,20 @@ async def on_member_remove(member):
     channel = client.get_channel(goodbye_channel_id)
     await channel.send("Goodbye!")
 
+@client.command(pass_context=True)
+async def join(ctx):
+    if (ctx.author.voice):
+        channel = ctx.message.author.voice.channel
+        await channel.connect()
+    else:
+        await ctx.send("You are not in a voice channel! Please join one, then try again.")
+
+@client.command(pass_context=True)
+async def leave(ctx):
+    if (ctx.voice_client):
+        await ctx.guild.voice_client.disconnect()
+        await ctx.send("Successfully left the voice channel.")
+    else:
+        await ctx.send("I am not in a voice channel!")
+
 client.run(token)
