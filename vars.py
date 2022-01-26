98b4c2c73ad92883d8902dc708c11264fbaa387f
0xCUBE <94565160+0xCUB3@users.noreply.github.com>
2022-01-26 07:00:38 -0500

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,5 +1,5 @@
 # Variables Used by the Bot
 prefix = '$'
-token = 'OTMyNzAzNzA0NDU5NjYxMzMy.YeW2Aw.PCYy840HyqRbLCOWjY-nynfOlck'
+token = ''
 welcome_channel_id = 932713007761489963
 goodbye_channel_id = 932713007761489963
